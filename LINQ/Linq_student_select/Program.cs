4d1e3cb8148067ecad146a4d3cb9584c0acf2bce
AlekseevSpartak <83302280+AlekseevSpartak@users.noreply.github.com>
2021-07-27 16:14:46 +0300

    Add files via upload

---
+++ LINQ/Linq_student_select/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Linq_Student
+{
+    class Program
+    {
+        public class Student
+        {
+            public string First { get; set; }
+            public string Last { get; set; }
+            public int ID { get; set; }
+            public List<int> Scores;
+        }
+
+        static List<Student> students = new List<Student>
+{
+
+   new Student {First="Svetlana", Last="Omelchenko", ID=111, Scores= new List<int> {97, 92, 81, 60}},
+   new Student {First="Claire", Last="O’Donnell", ID=112, Scores= new List<int> {75, 84, 91, 39}},
+};
+
+        static void Main(string[] args)
+        {
+            var studentQuery8 =     from student in students let x = student.Scores[0] + student.Scores[1] +    student.Scores[2] + student.Scores[3]
+     where x > averageScore select new { id = student.ID, score = x };
+
+            foreach (var item in studentQuery8)
+            {
+                Console.WriteLine("Student ID: {0}, Score: {1}", item.id, item.score);
+            }
+
+        }
+    }
+        }
+    
