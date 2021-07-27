d0a14c4626a5ac0b397f06b7763e1892c95559b2
AlekseevSpartak <83302280+AlekseevSpartak@users.noreply.github.com>
2021-07-27 16:15:23 +0300

    Add files via upload

---
+++ LINQ/Linq_student_var1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Linq_Student
+{
+    class Program
+    {
+        public class Student
+        {
+            public string First { get; set; }
+            public string Last { get; set; }
+            public int ID { get; set; }
+            public List<int> Scores;
+        }
+
+        static List<Student> students = new List<Student>
+{
+
+   new Student {First="Svetlana", Last="Omelchenko", ID=111, Scores= new List<int> {97, 92, 81, 60}},
+   new Student {First="Claire", Last="O’Donnell", ID=112, Scores= new List<int> {75, 84, 91, 39}},
+};
+
+        static void Main(string[] args)
+        {
+            var studentQuery2 = from student in students group student by student.Last[0];
+
+
+
+            foreach (var studentGroup in studentQuery2)
+            {
+                Console.WriteLine(studentGroup.Key);
+
+                foreach (Student student in studentGroup)
+                {
+                    Console.WriteLine("   {0}, {1}", student.Last, student.First);
+                }
+            }
+
+
+        }
+    }
+}
