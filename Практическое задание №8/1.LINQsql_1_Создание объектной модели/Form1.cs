44ae2e69666947f41d54190a5dba2eacf122e05e
AlekseevSpartak <83302280+AlekseevSpartak@users.noreply.github.com>
2021-07-28 15:34:14 +0300

    Add files via upload

---
+++ Практическое задание №8/1.LINQsql_1_Создание объектной модели/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Linq;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQsql_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataContext db = new DataContext (@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Спартак\source\repos\LINQsql_1\Northwind.mdf;Integrated Security=True;Connect Timeout=30");
+            var results = from c in db.GetTable<Customer>()
+                          where c.City == "London"
+                          select c;
+            foreach (var c in results)
+                listBox1.Items.Add(c.ToString());
+
+        }
+    }
+}
---
+++ Практическое задание №8/2.LINQsql_1_Создание связей в базе данных/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Linq;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQsql_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataContext db = new DataContext (@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Спартак\source\repos\LINQsql_1\Northwind.mdf;Integrated Security=True;Connect Timeout=30");
+           
+            
+            var custQuery =from cust in db.GetTable<Customer>()where cust.Orders.Any()select cust;
+            foreach (var custObj in custQuery)
+            {
+                ListViewItem item =
+                    listView1.Items.Add(custObj.CustomerID.ToString());
+                item.SubItems.Add(custObj.City.ToString());
+                item.SubItems.Add(custObj.Orders.Count.ToString());
+            }
+
+
+        }
+    }
+}
